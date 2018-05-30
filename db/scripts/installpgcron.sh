be38f80c45521042e7e1860816d6171e095cb15b
Brandon Bergenroth <bbergenroth@rti.org>
2018-05-30 14:56:29 -0400

    Change copy mode

--- db/scripts/installpgcron.sh
+++
@@ -1,5 +0,0 @@
-echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
-echo "cron.database_name = 'ingest'" >> /var/lib/postgresql/data/postgresql.conf
-echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
-pg_ctl restart
-psql -d ingest -c "CREATE EXTENSION pg_cron"
