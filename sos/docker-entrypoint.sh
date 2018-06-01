2afc0fe3330ed3e320ac54983272c3e37d0fb9b2
Brandon Bergenroth <bbergenroth@rti.org>
2018-06-01 10:34:10 -0400

    Add script to update SOS configuration from csv

--- sos/docker-entrypoint.sh
+++ sos/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python /tmp/updateconfig.py
 cp /tmp/configuration.db /usr/local/tomcat/webapps/52n-sos-webapp/configuration.db
 cp /tmp/datasource.properties /usr/local/tomcat/webapps/52n-sos-webapp/WEB-INF/datasource.properties
 $CATALINA_HOME/bin/catalina.sh run
