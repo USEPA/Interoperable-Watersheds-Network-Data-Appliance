fbae9923174af3abb6ede6e3ccacbc029fef1178
Brandon Bergenroth <bbergenroth@rti.org>
2018-06-01 13:45:30 -0400

    Replace updateconfig.py with bash script

--- sos/docker-entrypoint.sh
+++ sos/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python /tmp/updateconfig.py
+/tmp/updateconfig.sh
 cp /tmp/configuration.db /usr/local/tomcat/webapps/52n-sos-webapp/configuration.db
 cp /tmp/datasource.properties /usr/local/tomcat/webapps/52n-sos-webapp/WEB-INF/datasource.properties
 $CATALINA_HOME/bin/catalina.sh run
