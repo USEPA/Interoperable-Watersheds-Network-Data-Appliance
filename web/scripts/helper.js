8f17d0e4e13e85a3b64d2597f0ebce7c0f7fb9bd
Jennifer Wellard <jwellard@rti.org>
2018-06-06 10:17:16 -0400

    - initial commit of web UI files - add call to sensors service, populate sensors table

---
+++ web/scripts/helper.js
@@ -0,0 +1,30 @@
+﻿var helper = {
+
+    callService: function (url, data, successFunction) {
+
+        //make ajax call
+        var promise = $.ajax({
+            type: "GET",
+            url: url,
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        });
+
+        //success
+        promise.done(successFunction);
+
+        //failure
+        promise.fail(function (xhr, status, error) {
+            console.log("xhr.status: " + xhr.status);
+            console.log("xhr.readyState: " + xhr.readyState);
+            console.log("xhr.responseText: " + xhr.responseText);
+            console.log("xhr.responseXML: " + xhr.responseXML);
+            console.log("status: " + status);
+            console.log("error: " + error);
+            console.log("url: " + url);
+            console.log("data: " + data);
+        });
+
+    }
+};
