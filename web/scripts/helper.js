74bcc4f9d6b5e87ca8666225301285be69a80576
Jennifer Wellard <jwellard@rti.org>
2018-06-06 12:13:09 -0400

    - cleaned up and documented code - added loading spinner and error options to helper code

--- web/scripts/helper.js
+++ web/scripts/helper.js
@@ -1,10 +1,31 @@
 ﻿var helper = {
 
-    callService: function (url, data, successFunction) {
+    callService: function (url, data, verb, successFunction, options) {
+        //makes an AJAX call to a service, handles success and failure
+
+        var defaults = {
+            showLoading: true, //boolean - show loading spinner
+            loadingElementId: "loading", //string - the id of the loading spinner element
+            logErrorToConsole: true, //boolean - log errors to the browser console
+            showFriendlyErrorAlert: false, //boolean - show a friendly error alert
+            showVerboseErrorAlert: false //boolean - show a verbose error alert
+        }
+
+        if (options) {
+            defaults.showLoading = options.showLoading || defaults.showLoading;
+            defaults.loadingElementId = options.loadingElementId || defaults.loadingElementId;
+            defaults.logErrorToConsole = options.logErrorToConsole || defaults.logErrorToConsole;
+            defaults.showFriendlyErrorAlert = options.showFriendlyErrorAlert || defaults.showFriendlyErrorAlert;
+            defaults.showVerboseErrorAlert = options.showVerboseErrorAlert || defaults.showVerboseErrorAlert;
+        }
+
+        if (defaults.showLoading) {
+            $("#" + defaults.loadingElementId).show();
+        }
 
         //make ajax call
         var promise = $.ajax({
-            type: "GET",
+            type: verb,
             url: url,
             data: data,
             contentType: "application/json; charset=utf-8",
@@ -16,14 +37,30 @@
 
         //failure
         promise.fail(function (xhr, status, error) {
-            console.log("xhr.status: " + xhr.status);
-            console.log("xhr.readyState: " + xhr.readyState);
-            console.log("xhr.responseText: " + xhr.responseText);
-            console.log("xhr.responseXML: " + xhr.responseXML);
-            console.log("status: " + status);
-            console.log("error: " + error);
-            console.log("url: " + url);
-            console.log("data: " + data);
+
+            if (defaults.logErrorToConsole) {
+                console.log("xhr.status: " + xhr.status);
+                console.log("xhr.readyState: " + xhr.readyState);
+                console.log("xhr.responseText: " + xhr.responseText);
+                console.log("xhr.responseXML: " + xhr.responseXML);
+                console.log("status: " + status);
+                console.log("error: " + error);
+                console.log("url: " + url);
+                console.log("data: " + data);
+            }
+            if (defaults.showFriendlyErrorAlert) {
+                alert("An error occurred when attempting to call a service.")
+            }
+            if (defaults.showVerboseErrorAlert) {
+                alert("An error occurred when attempting to call a service.\nThe error details are as follows.\nxhr.status: " + xhr.status + "\nxhr.readyState: " + xhr.readyState + "\nxhr.responseText: " + xhr.responseText + "\nxhr.responseXML: " + xhr.responseXML + "\nstatus: " + status + "\nerror: " + error + "\nurl: " + url + "\ndata: " + data);
+            }
+        });
+
+        //always
+        promise.always(function () {
+            if (defaults.showLoading) {
+                $("#" + defaults.loadingElementId).hide();
+            }
         });
 
     }
--- web/scripts/ingest.js
+++ web/scripts/ingest.js
@@ -6,26 +6,42 @@
             successTemplate: "<i class='fas fa-arrow-circle-up text-success'></i>",
             unknownTemplate: "<i class='fas fa-exclamation-triangle text-warning'></i>",
             failureTemplate: "<i class='fas fa-arrow-circle-down text-danger'></i>",
-            sensorIdLinkTemplate: "<a href='javascript:void(0)' onclick='ingest.showSensorModal([sensor_id]);'>[sensor_id]</a>"
+            sensorIdLinkTemplate: "<a href='javascript:void(0)' onclick='ingest.editSensor([sensor_id]);'>[sensor_id]</a>"
+        },
+        services: {
+            sensors: "http://havasu.rtp.rti.org:8000/sensors/"
         }
     },
-    init: function() {
-        helper.callService("http://havasu.rtp.rti.org:8000/sensors/", "{}", function (data) {
-            //ingest.populateSensorsTable(data);
+    init: function () {
+        //initialize page values and settings
+
+        var me = ingest;
+        var g = me.globals;
+        var s = g.services;
+
+        //call sensors service and populate sensors table
+        helper.callService(s.sensors, "{}", "GET", function (data) {
             $("#tblSensors").bootstrapTable({ data: data });
         });
+
+        //reset sensors form when form is hidden
         $("#sensorModal").on("hidden.bs.modal", function () {
             ingest.resetSensorModal();
         });
     },
-    populateSelect: function (selectId) {
+    populateSelect: function (selectId, data, nameField, idField) {
+        //populates a select element with name and id from json object
+
         var ddl = $("#" + selectId);
         ddl.append($("<option />").val("").text(""));
-        $.each(result.d, function () {
-            ddl.append($("<option />").val(this.ProjectEmsId).text(this.ProjectName));
+        $.each(data, function () {
+            ddl.append($("<option />").val(this[idField]).text(this[nameField]));
         });
     },
-    sensorIdFormatter: function(value) {
+    sensorIdFormatter: function (value) {
+        //bootstrap-table formatter for sensor id column of sensors table
+        //makes sensor id a link to launch edit sensor form
+
         var me = ingest;
         var g = me.globals;
         var t = g.templates;
@@ -33,7 +49,10 @@
         var link = t.sensorIdLinkTemplate.replace(/\[sensor_id\]/g, value);
         return link;
     },
-    dateFormatter: function(value) {
+    dateFormatter: function (value) {
+        //bootstrap-table formatter for a date column
+        //converts date to en-US locale date/time string
+
         var formattedDate = " ";
         if (value != null) {
             var rawDate = new Date(value);
@@ -42,6 +61,9 @@
         return formattedDate;
     },
     qcRulesApplyFormatter: function (value) {
+        //boostrap-table formatter for qc rules apply column of sensors table
+        //replaces column text with an icon based on text value
+
         var me = ingest;
         var g = me.globals;
         var t = g.templates;
@@ -53,6 +75,9 @@
         return icon;
     },
     ingestStatusFormatter: function (value) {
+        //boostrap-table formatter for ingest status column of sensors table
+        //replaces column text with an icon based on text value
+
         var me = ingest;
         var g = me.globals;
         var t = g.templates;
@@ -68,19 +93,24 @@
         }
         return icon;
     },
-    showSensorModal: function (sensorId) {
+    editSensor: function (sensorId) {
+        //launch edit sensor form and populate with existing sensor data based on sensor id
+
         $("#sensorId").val(sensorId);
         $("#sensorId").prop("disabled", true);
         $("#sensorModal").modal("show");
-        //retrieve sensor data here
+        //*** retrieve sensor data from service here ***
     },
     resetSensorModal: function () {
-        //reset sensor modal fields
+        //reset sensor form
+
         $("#sensorId").val("");
         $("#sensorId").prop("disabled", false);
     }
 };
 
 $("document").ready(function () {
+    //runs when page loads
+
     ingest.init();
 });
