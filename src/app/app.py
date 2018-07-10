d70d5da12a74d07e1a6a0f890ad61a99ae5ec4fb
Adam Shelton <adamshelton@rti.org>
2018-07-10 11:49:00 -0400

    remote debugging works ptvsd upstream issue

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,10 @@
 from utils import factory
 
 application = factory.bootstrap_app()
-
+if application.config['DEBUG']:
+    import ptvsd, time
+    ptvsd.enable_attach(secret=None,address=('0.0.0.0',3000))
+    time.sleep(10)
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
 
