94491ff3710bfef6c32c4fb0af856fd87b5213f8
Brandon Bergenroth <bbergenroth@rti.org>
2018-10-19 08:27:00 -0400

    Add new r10 sensors

--- web/scripts/config.js
+++ web/scripts/config.js
@@ -1,6 +1,8 @@
 ﻿var config = {
+    //*** local ***
+    serviceUrl : "http://ingest_api:8000"
     //*** development ***
     //serviceUrl: "http://havasu.rtp.rti.org:8000/"
     //*** production ***
-    serviceUrl: "http://ec2-34-228-169-152.compute-1.amazonaws.com/api/"
+    //serviceUrl: "http://ec2-34-228-169-152.compute-1.amazonaws.com/api/"
 }
